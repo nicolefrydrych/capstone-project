ff40009d26f10ff64ee6a9beda4c3f6c6ff0da51
nicole.frydrych <nicole.fr91@gmx.de>
2020-03-05 15:49:54 +0100

    backup from master

---
+++ src/pages/Bookmark.stories.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Bookmark from './Bookmark'
+import { action } from '@storybook/addon-actions'
+
+export default {
+  title: 'pages/Bookmark',
+  component: Bookmark,
+}
+
+const styles = {
+  border: '1px solid blue',
+  position: 'absolute',
+  left: '50%',
+  top: '50%',
+  transform: 'translate("-50", "-50")',
+}
+
+export const standardBookmark = () => (
+  <div style={styles}>
+    <Bookmark
+      id="test"
+      onBookmarkClick={action('bookmarkClick')}
+      isBookmarked="true"
+    />
+  </div>
+)
