5ac1d48643cc6c45ae52d73a3820aef3ece6c014
nicole.frydrych <nicole.fr91@gmx.de>
2020-03-13 16:18:51 +0100

    Add new styling to form

--- src/pages/CreateRecipe.js
+++ src/pages/CreateRecipe.js
@@ -44,6 +44,43 @@ export default function Fridge({ addRecipe }) {
             placeholder="Products"
           />
         </LabelStyled>
+        <UlStyled>
+          <LiStyled>
+            <LabStyled htmlFor="name">Recipe name</LabStyled>
+            <InputtStyled
+              maxLength="25"
+              ref={register()}
+              autoFocus
+              type="text"
+              name="name"
+              id="name"
+            />
+          </LiStyled>
+          <LiStyled>
+            <LabStyled htmlFor="instruction">Instruction</LabStyled>
+            <InputtStyled
+              maxLength="120"
+              ref={register()}
+              height="70"
+              type="text"
+              name="instruction"
+              id="instruction"
+            />
+            <SpannStyled>Enter the instruction</SpannStyled>
+          </LiStyled>
+          <LiStyled>
+            <LabStyled htmlFor="products">Products</LabStyled>
+            <InputtStyled
+              maxLength="120"
+              ref={register()}
+              height="70"
+              type="text"
+              name="instruction"
+              id="instruction"
+            />
+            <SpannStyled>Enter the products you need</SpannStyled>
+          </LiStyled>
+        </UlStyled>
         <StyledDiv>
           <ButtonStyled type="submit">Submit</ButtonStyled>
           <ButtonStyled onClick={deleteInputFields}>Cancel</ButtonStyled>
@@ -75,6 +112,63 @@ export default function Fridge({ addRecipe }) {
   //   })
   // }
 }
+
+const InputtStyled = styled.input`
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  width: 100%;
+  display: block;
+  outline: none;
+  border: none;
+  height: 25px;
+  line-height: 25px;
+  font-size: 16px;
+  padding: 0;
+  background: white;
+  font-family: Georgia, 'Times New Roman', Times, serif;
+  height: ${props => props.height || 35}px;
+`
+
+const SpannStyled = styled.span`
+  background: #ebe6e8;
+  display: block;
+  padding: 3px;
+  width: 280px;
+  text-align: center;
+  color: gray;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 16px;
+`
+
+const LabStyled = styled.label`
+  display: block;
+  float: left;
+  margin-top: -27px;
+  margin-bottom: 17px;
+  background: #ffffff;
+  height: 22px;
+  padding: 2px 5px 2px 5px;
+  color: #b9b9b9;
+  font-size: 14px;
+  overflow: hidden;
+  font-family: Arial, Helvetica, sans-serif;
+`
+
+const UlStyled = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`
+const LiStyled = styled.li`
+  margin-top: 30px;
+  display: block;
+  background: #f5f5f5;
+  padding: 15px 15px 25px;
+  border: 1px solid #dddddd;
+  margin-bottom: 30px;
+  border-radius: 10px;
+`
 
 const CardForm = styled.form`
   margin-top: 70px;
