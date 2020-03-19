9ed6044b6b03cf038609f72d3aafb215eb1df49c
nicole.frydrych <nicole.fr91@gmx.de>
2020-03-19 10:19:23 +0100

    Create Feature/random_hint branch

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,23 +1,60 @@
 import React from 'react'
 import styled from 'styled-components'
 import { NavLink } from 'react-router-dom'
+import hintsData from '../hint'
 
 export default function StartPage() {
+  const randomNumber = Math.floor(Math.random(1) * 2)
+
+  const veganHints = hintsData[Math.floor(Math.random() * hintsData.length)]
+
   return (
     <StartPageContainer>
       <LinkStyled to="/chooseameal">
         <FirstDivStyled>
+          {randomNumber === 0 && (
+            <HintStyledLeft>Tip of the day: {veganHints}</HintStyledLeft>
+          )}
           <DivStyled>Are you hungry? Let's cook! </DivStyled>
         </FirstDivStyled>
       </LinkStyled>
       <LinkStyled to="/shoppinglist">
         <SecondDivStyled>
+          {randomNumber === 1 && (
+            <HintStyledRight>Tip of the day: {veganHints}</HintStyledRight>
+          )}
           <DivStyled>Let's go shopping!</DivStyled>
         </SecondDivStyled>
       </LinkStyled>
     </StartPageContainer>
   )
 }
+
+const HintStyledRight = styled.p`
+  position: absolute;
+  top: 16px;
+  right: 16px;
+  border: 1px solid #baafb0;
+  text-align: center;
+  border-radius: 50px 10px 50px 50px;
+  padding: 20px 14px;
+  width: 140px;
+  background-color: #d3e0d7;
+  color: #9e9393;
+`
+
+const HintStyledLeft = styled.p`
+  position: absolute;
+  top: 16px;
+  left: 16px;
+  border: 1px solid #baafb0;
+  text-align: center;
+  border-radius: 10px 50px 50px 50px;
+  padding: 20px 14px;
+  width: 140px;
+  background-color: #e6d4d7;
+  color: #9e9393;
+`
 
 const StartPageContainer = styled.div`
   display: grid;
@@ -28,6 +65,7 @@ const StartPageContainer = styled.div`
 `
 
 const FirstDivStyled = styled.section`
+  position: relative;
   width: auto;
   height: 100%;
   background-color: #bda8b2;
@@ -37,6 +75,7 @@ const FirstDivStyled = styled.section`
 `
 
 const SecondDivStyled = styled.section`
+  position: relative;
   width: auto;
   height: 100%;
   background-color: #a7bdaf;
